d25deee3ff86a4529e1c43574a1c74620a7c2ba9
Fu Cheng <alexcheng1982@gmail.com>
2025-04-02 19:48:16 +1300

    enable java test

--- executors/java/src/test/kotlin/com/javaaidev/llmcodeexecutor/executor/java/JavaCodeExecutorTest.kt
+++ executors/java/src/test/kotlin/com/javaaidev/llmcodeexecutor/executor/java/JavaCodeExecutorTest.kt
@@ -2,18 +2,16 @@ package com.javaaidev.llmcodeexecutor.executor.java
 
 import com.javaaidev.llmcodeexecutor.core.CodeExecutionRequest
 import org.junit.jupiter.api.Test
-import kotlin.test.Ignore
 
 class JavaCodeExecutorTest {
     @Test
-    @Ignore
     fun basic() {
         val result = JavaCodeExecutor.execute(
             CodeExecutionRequest(
                 """
         public class Main {
             public static void main(String[] args) {
-                System.out.println("hello");
+                System.out.println("Hello");
             }
         }
     """.trimIndent()
