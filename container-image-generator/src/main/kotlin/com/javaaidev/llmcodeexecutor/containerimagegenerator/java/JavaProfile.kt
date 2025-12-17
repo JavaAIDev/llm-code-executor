e518433e94add1005f0a3a83aafe1665e8b7bb60
Fu Cheng <alexcheng1982@gmail.com>
2025-12-17 22:15:26 +1300

    update docker image

--- container-image-generator/src/main/kotlin/com/javaaidev/llmcodeexecutor/containerimagegenerator/java/JavaProfile.kt
+++ container-image-generator/src/main/kotlin/com/javaaidev/llmcodeexecutor/containerimagegenerator/java/JavaProfile.kt
@@ -1,9 +1,14 @@
 package com.javaaidev.llmcodeexecutor.containerimagegenerator.java
 
-data class JavaProfile(val javaVersion: String, val dependencies: List<MavenDependency>)
+data class JavaProfile(val javaVersion: String, val dependencies: List<MavenDependency>) {
+    val description: String
+        get() = "Java $javaVersion with libraries: ${dependencies.joinToString(", ") { it.coordinate() }}"
+}
 
 data class MavenDependency(
     val groupId: String,
     val artifactId: String,
     val artifactVersion: String
 )
+
+fun MavenDependency.coordinate() = "$groupId:$artifactId:$artifactVersion"
--- container-image-generator/src/main/kotlin/com/javaaidev/llmcodeexecutor/containerimagegenerator/python/PythonProfile.kt
+++ container-image-generator/src/main/kotlin/com/javaaidev/llmcodeexecutor/containerimagegenerator/python/PythonProfile.kt
@@ -3,4 +3,7 @@ package com.javaaidev.llmcodeexecutor.containerimagegenerator.python
 data class PythonProfile(
     val pythonVersion: String,
     val dependencies: List<String>,
-)
+) {
+    val description: String
+        get() = "Python $pythonVersion with libraries: ${dependencies.joinToString(", ")}"
+}
